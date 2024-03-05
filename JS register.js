2158facbd17a54ccdac47bb4208906fae17a3257
theapedronan <theaangelacaramat08@gmail.com>
2024-03-05 14:46:25 +0800

    second commit

--- JS register.js
+++ JS register.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+//console.log("Hello World");
 
 // string - "Thea", 'Thea'. "08"
 // number - 08, 24.1 1230e
@@ -10,21 +10,38 @@ console.log("Hello World");
             // [1, 2, 3]
             // ["apple", "banana", "grape"],
             //]
-console.log("Hello World"), 123, true, ["apple", "banana", "grape"];
+//console.log("Hello World"), 123, true, ["apple", "banana", "grape"];
 // console.error("Hello World");         
 // console.warn("Hello World");
 // console.table(["apple", "banana", "grape"]);
 // console.log(typeof)(name)
 
 let txtUsername = document.querySelector("#txtUsername")
+let txtPassword = document.querySelector("#txtPassword")
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword")
 let btnRegister = document.querySelector("#btnRegister")
 
 btnRegister.onclick = function () {
-     register(txtUsername.value)
+     register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
 }
 
-function register (username) { 
-    console.log(username)
+function register (username, password, confirmPassword) { 
+    // if...else if...else
+    // Conditional Operators (Greater than >, Less Than <, Equal ==.Not !,)
+    //Greater Than or Equal >=, Less Than  or Equal <=
+    // Not Equal !=
+
+    //console.log(username, password, confirmPassword)
+
+    if(password == confirmPassword && password != "" && confirmPassword != "") {
+       return console.log("Password match!")
+    } else if (password == "" || confirmPassword == "") {
+       return console.log("Password must not be empty.")
+    }
+     else {
+       return console.log("Password does not match.")
+    }
+    
 } 
 
 
